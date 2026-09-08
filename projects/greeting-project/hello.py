e27b8f460323f9f806bcd7041ed7d5c80e4940e6
gn08 <79935615+gn08@users.noreply.github.com>
2026-09-08 12:24:23 -0700

    Update hello.py

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,6 +1,7 @@
 # My first Python project in GitHub
 def greet_user():
- name = input("Enter your name: ")
- print(f"Hello, {name}! Welcome to Data Science.")
+ first_name = input("Enter your first name: ")
+ last_name = input("Enter your last name: ")
+ print(f"Hello, {first_name} {last_name}! Welcome to Data Science.")
 if __name__ == "__main__":
  greet_user()
