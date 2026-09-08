6af3799972b034b0c2f82a80adbff5bf402581d9
gn08 <79935615+gn08@users.noreply.github.com>
2026-09-08 12:31:33 -0700

    Update hello.py
    This is my updated code. The commit before this adds a first and last name variable, which prompts the user to input their first and last name. The print statement then outputs the greeting utilizing the first and last name entered.

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,7 +1,7 @@
 # My first Python project in GitHub
 def greet_user():
  first_name = input("Enter your first name: ")
- last_name = input("Enter your last name: ")
+ last_name = input("Enter your last name: ") 
  print(f"Hello, {first_name} {last_name}! Welcome to Data Science.")
 if __name__ == "__main__":
  greet_user()
